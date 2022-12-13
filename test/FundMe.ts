b1fa67a4dbfff01241c8bb112fd7c7d2577c7959
Eric <erictes96@gmail.com>
2022-12-13 22:00:04 +0800

    Add spacing to comments

--- test/FundMe.ts
+++ test/FundMe.ts
@@ -9,9 +9,11 @@ describe("FundMe", function() {
 
     // deployment has a fixture function that runs all the deploy script based on the input tags
     await deployments.fixture(["all"]);
-    // The first param gives us the most recently deployed FundMe contract. This comes from hardhat-deploy package
-    // The second param, deployer, is saying we want the FundKMe contract to link to the deployer.
+
+    // The first param gives us the most recently deployed FundMe contract. This comes from hardhat-deploy package when we ran the code above.
+    // The second param, deployer, is saying we want the FundMe contract to link to the deployer.
     fundMe = await ethers.getContract("FundMe", deployer);
+
     mockV3Aggregator = await ethers.getContract("MockV3Aggregator", deployer);
 
     // Below code is another way of grabbing the accounts from the network field in the hardhat config file
